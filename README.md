b49a2c5f9e4185803eff9ecc349221346edd2609
Carlos <celiberato1@outlook.com>
2023-08-01 21:06:00 +0000

    cicd

---
+++ README.md
@@ -0,0 +1,1 @@
+# demo-funcional-spring-azure
---
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/produtos", produces = { MediaType.APPLICATION_JSON_VALUE})
+public class ProdutoController {
+
+	
+	@GetMapping(value = "/all")
+	@ResponseStatus(HttpStatus.OK)
+	public String findAll() throws Exception {
+		return "MICROSERVIÇO DEMONSTRAÇÃO!!!";
+	}
+
+}
